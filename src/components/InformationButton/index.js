a0a1f6a992c5f64fc473e716362e826785cdb138
nbkhope <renan@emproveit.com>
2018-06-09 20:57:35 -0700

    em-2223 Add InformationButton component

---
+++ src/components/InformationButton/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import IconInfoOutline from '@material-ui/icons/InfoOutline';
+import { connect } from 'react-redux';
+import { show as showModal } from 'redux-modal';
+
+import './styles.scss';
+
+class InformationButton extends Component {
+  handleClick = () => {
+    this.props.showModal('informationModal', this.props.information);
+  };
+
+  render() {
+    return (
+      <IconButton className="information-button" onClick={this.handleContractTypeInfoClick}>
+        <IconInfoOutline />
+      </IconButton>
+    );
+  }
+}
+
+InformationButton.propTypes = {
+  information: PropTypes.object.isRequired,
+};
+
+const actions = {
+  showModal,
+};
+
+export default connect(null, actions)(InformationButton);
