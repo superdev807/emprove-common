12525eff18a120901069486f5fb735f01b7530be
nbkhope <renan@emproveit.com>
2018-06-13 10:46:38 -0700

    em-2255 Change FooterList font sizes

--- src/components/FooterList/index.js
+++ src/components/FooterList/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import React from 'react';
+import cx from 'classnames';
 
 import './styles.scss';
 
 const FooterList = (props) => {
   return (
-    <ul className="footer-list">
+    <ul className={cx('footer-list', props.className)}>
       {props.items.map(item => <li className="footer-list__item">{item}</li>)}
     </ul>
   );
