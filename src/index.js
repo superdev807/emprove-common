df085f2e40a776f5a62f733dcbf021861290a20e
nbkhope <renan@emproveit.com>
2018-06-12 19:11:31 -0700

    em-2255 Include Footer in index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict';
 
+export { default as Footer } from './components/Footer';
 export { default as InformationButton } from './components/InformationButton';
 export { default as InformationModal } from './components/InformationModal';
