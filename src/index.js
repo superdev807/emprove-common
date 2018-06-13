fdc7863fa76a35c4944e28b97ae9361758a4d7b9
juanortiz10 <juanjo.jjo94@gmail.com>
2018-06-13 15:07:51 -0500

    Resolve conflicts and add use strict

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 
 export { default as InformationButton } from './components/InformationButton';
 export { default as InformationModal } from './components/InformationModal';
-<<<<<<< HEAD
 export { default as PrivacyPolicy } from './components/PrivacyPolicy';
 export { default as TermsOfService } from './components/TermsOfService';
-=======
 export { default as DatePickerField } from './components/DatePickerField';
->>>>>>> 4006b772e483755040ba413a47097d20840beb7a
