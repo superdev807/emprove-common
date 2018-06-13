e51dd3114b9c0c5af65468a2f665918611a267db
nbkhope <renan@emproveit.com>
2018-06-13 11:27:35 -0700

    em-2255 Add SocialMediaLinks component

---
+++ src/components/SocialMediaLinks/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import IconFacebook from '~/icons/IconFacebookLetter';
+import IconTwitter from '~/icons/IconTwitter';
+import IconLinkedIn from '~/icons/IconLinkedIn';
+import IconInstagram from '~/icons/IconInstagram';
+import './styles.scss';
+
+const SocialMediaLinks = (props) => {
+  return (
+    <div className="social-media-links">
+      <a href="https://www.facebook.com/emproveit/" target="_blank" rel="noopener noreferrer">
+        <IconFacebook className="social-media-links__icon" />
+      </a>
+      <a href="https://twitter.com/emprove_it" target="_blank" rel="noopener noreferrer">
+        <IconTwitter className="social-media-links__icon" />
+      </a>
+      <a href="https://www.instagram.com/emprove_it/" target="_blank" rel="noopener noreferrer">
+        <IconInstagram className="social-media-links__icon" />
+      </a>
+      <a href="https://www.linkedin.com/company/emprove-inc/" target="_blank" rel="noopener noreferrer">
+        <IconLinkedIn className="social-media-links__icon" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaLinks;
