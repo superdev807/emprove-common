b30aa453eb7a271a4385f2ac90287859fe2667ab
nbkhope <renan@emproveit.com>
2018-06-15 14:51:14 -0700

    em-2192 Finish AboutUsHero draft

--- src/components/AboutUs/components/AboutUsHero/index.js
+++ src/components/AboutUs/components/AboutUsHero/index.js
@@ -2,10 +2,14 @@
 
 import React from 'react';
 
+import './styles.scss';
+
 const AboutUsHero = (props) => {
   return (
-    <section>
-      Home Improvement Renewed
+    <section className="about-us-hero">
+      <div className="about-us-hero__title">
+        Home Improvement <strong>Renewed</strong>
+      </div>
     </section>
   );
 };
