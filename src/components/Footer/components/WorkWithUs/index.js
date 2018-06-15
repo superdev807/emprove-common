564efe159f01a185778305307e0afaeb053a007d
nbkhope <renan@emproveit.com>
2018-06-14 18:07:16 -0700

    em-2255 Add initial WorkWithUs component

---
+++ src/components/Footer/components/WorkWithUs/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import React from 'react';
+
+const WorkWithUs = (props) => {
+  return (
+    <div>
+      Why work with us?
+
+      we have the most qualified consumers
+
+      you can bid free and efficently
+
+      Grow your business with us
+
+      build your professional brand
+
+      lower your overhead costs
+    </div>
+  );
+};
+
+export default WorkWithUs;
