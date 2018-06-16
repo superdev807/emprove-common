0fd41ee03e81b3adda47580d244b2f13d2ab5761
nbkhope <renan@emproveit.com>
2018-06-16 16:30:54 -0700

    em-2192 Almost finish Our Product section

--- src/components/AboutUs/components/OurProduct/ProductConcept.js
+++ src/components/AboutUs/components/OurProduct/ProductConcept.js
@@ -1,14 +1,24 @@
 'use strict';
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './product-concept-styles.scss';
 
 const ProductConcept = (props) => {
   return (
-    <div>
-      <div>{props.concept.name}</div>
-      <p>{props.concept.description}</p>
+    <div className={cx('product-concept', props.className)}>
+      <div className="product-concept__icon">{props.concept.icon}</div>
+      <div className="product-concept__name">{props.concept.name}</div>
+      <p className="product-concept__description">{props.concept.description}</p>
     </div>
   );
+};
+
+ProductConcept.propTypes = {
+  concept: PropTypes.object.isRequired,
+  className: PropTypes.string,
 };
 
 export default ProductConcept;
--- src/components/AboutUs/components/OurProduct/index.js
+++ src/components/AboutUs/components/OurProduct/index.js
@@ -3,33 +3,39 @@
 import React from 'react';
 
 import ProductConcept from './ProductConcept';
+import IconBidIt from '~/icons/IconBidIt';
+import IconCostIt from '~/icons/IconCostIt';
+// import IconDreamIt from '~/icons/IconDreamIt';
+import './styles.scss';
 
 const productConcepts = [
   {
     id: 1,
-    name: 'Dream It!',
+    name: <span><strong>Dream</strong> It!</span>,
     description: 'Emprove empowers home owners to dream with purpose and precision.  The platforms learns home owner objectives and design sensibilities then translates them into an actionable project plan.',
-    icon: null,
+    icon: <IconCostIt />,
   },
   {
     id: 2,
-    name: 'Cost It!',
+    name: <span><strong>Cost</strong> It</span>,
     description: 'Emprove’s proprietary data engine generates a comprehensive scope and cost estimate for the project.  Each scope element can be tailored to accommodate various project scales or budget constraints.',
     icon: null,
   },
   {
     id: 3,
-    name: 'Bid It!',
+    name: <span><strong>Bid</strong> It!</span>,
     description: 'Emprove delivers a detailed request-for-proposal to its marketplace of qualified contractors and administers a bid process.  A competitive market is achieved for home owners and contractors efficiently engage with the most qualified consumer in the market.',
-    icon: null,
+    icon: <IconBidIt />,
   },
 ];
 
 const OurProduct = (props) => {
   return (
-    <section>
-      <div>Our Product</div>
-      {productConcepts.map(concept => <ProductConcept key={concept.id} concept={concept} />)}
+    <section className="our-product">
+      <div className="our-product__title">Our <strong>Product</strong></div>
+      <div className="our-product__concepts">
+        {productConcepts.map(concept => <ProductConcept key={concept.id} className="our-product__concept" concept={concept} />)}
+      </div>
     </section>
   );
 };
