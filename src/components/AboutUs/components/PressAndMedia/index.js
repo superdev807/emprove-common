ee35f95581143d5db5d6584912de527d76783447
nbkhope <renan@emproveit.com>
2018-06-17 15:27:05 -0700

    em-2192 Finish Press and Media

--- src/components/AboutUs/components/PressAndMedia/index.js
+++ src/components/AboutUs/components/PressAndMedia/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import PressArticle from './PressArticle';
+import './styles.scss';
 
 const articles = [
   {
@@ -24,12 +25,14 @@ const articles = [
 
 const PressAndMedia = (props) => {
   return (
-    <section>
-      <div>Press & Media</div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis, tellus non aliquam gravida.</p>
-      {articles.map(article => (
-        <PressArticle key={article.id} article={article} />
-      ))}
+    <section className="press-and-media">
+      <div className="press-and-media__title">Press & <strong>Media</strong></div>
+      <p className="press-and-media__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis, tellus non aliquam gravida.</p>
+      <div className="press-and-media__articles">
+        {articles.map(article => (
+          <PressArticle className="press-and-media__article" key={article.id} article={article} />
+        ))}
+      </div>
     </section>
   );
 };
