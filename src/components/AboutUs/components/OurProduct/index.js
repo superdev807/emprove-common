6a31b6766ed3d051ef75f30e35e34133c951f9b0
nbkhope <renan@emproveit.com>
2018-06-17 14:20:14 -0700

    em-2192 Finish our product icon part

--- src/components/AboutUs/components/OurProduct/index.js
+++ src/components/AboutUs/components/OurProduct/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ProductConcept from './ProductConcept';
 import IconBidIt from '~/icons/IconBidIt';
 import IconCostIt from '~/icons/IconCostIt';
-// import IconDreamIt from '~/icons/IconDreamIt';
+import IconDreamIt from '../../../../icons/IconDreamIt';
 import './styles.scss';
 
 const productConcepts = [
@@ -13,13 +13,13 @@ const productConcepts = [
     id: 1,
     name: <span><strong>Dream</strong> It!</span>,
     description: 'Emprove empowers home owners to dream with purpose and precision.  The platforms learns home owner objectives and design sensibilities then translates them into an actionable project plan.',
-    icon: <IconCostIt />,
+    icon: <IconDreamIt />,
   },
   {
     id: 2,
     name: <span><strong>Cost</strong> It</span>,
     description: 'Emprove’s proprietary data engine generates a comprehensive scope and cost estimate for the project.  Each scope element can be tailored to accommodate various project scales or budget constraints.',
-    icon: null,
+    icon: <IconCostIt />,
   },
   {
     id: 3,
