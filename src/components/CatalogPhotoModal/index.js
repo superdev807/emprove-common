3f6f620ffbe5c13d972772006f3f20ba2de0c92b
nbkhope <renan@emproveit.com>
2018-06-20 15:57:01 -0700

    em-2361 Add catalog photo header, close button and dream it link

--- src/components/CatalogPhotoModal/index.js
+++ src/components/CatalogPhotoModal/index.js
@@ -3,7 +3,10 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
 
+import CloseButton from './components/CloseButton';
 import CatalogPhoto from './components/CatalogPhoto';
+import CatalogPhotoHeader from './components/CatalogPhotoHeader';
+import DreamItLink from './components/DreamItLink';
 import './styles.scss';
 
 function getModalStyle() {
@@ -19,21 +22,42 @@ function getModalStyle() {
 
 const CatalogPhotoModal = (props) => {
   console.log('CatalogPhotoModal', props);
-  if (!props.image) {
-    return null;
-  }
+  // if (!props.image) {
+  //   return null;
+  // }
 
+  const imageName = `${props.image.style} ${props.image.homeArea}`;
+
+  // <div className="catalog-photo-modal__catalog-photo-container">
   return (
     <Modal open={true || props.open} onClose={props.onClose}>
       <div className="catalog-photo-modal">
-        <CatalogPhoto className="catalog-photo-modal__catalog-photo" imageKey={props.image.imageKey} alt={props.image.name} />
-      </div>
+          <CatalogPhoto
+            className="catalog-photo-modal__catalog-photo"
+            imageKey={props.image.imageKey}
+            alt={imageName}
+          />
+          <CatalogPhotoHeader
+            className="catalog-photo-modal__catalog-photo-header"
+            designStyle={props.image.style}
+            homeArea={props.image.homeArea}
+            qualityStandard={props.image.quality}
+          />
+          <CloseButton className="catalog-photo-modal__close-button" onClick={props.onClose} />
+          <DreamItLink browseFilter={imageName.toLowerCase().replace(/ /g, '-')} />
+        </div>
     </Modal>
   );
+// </div>
 };
 
-// CatalogPhotoModal.propTypes = {
-//
-// }
+CatalogPhotoModal.defaultProps = {
+  image: {
+    imageKey: '',
+    style: 'Loading',
+    homeArea: '',
+    quality: 'We will show you the image soon'
+  }
+};
 
 export default CatalogPhotoModal;
