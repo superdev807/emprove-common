3217f21b47e26a93372bb8c3eb59dcdeda62608f
nbkhope <renan@emproveit.com>
2018-06-21 11:00:41 -0700

    em-2361 Add loading prop to CatalogPhotoHeader and display loading message in title

--- src/components/CatalogPhotoModal/components/CatalogPhotoHeader/index.js
+++ src/components/CatalogPhotoModal/components/CatalogPhotoHeader/index.js
@@ -10,16 +10,27 @@ const CatalogPhotoHeader = (props) => {
   return (
     <div className={cx('catalog-photo-header', props.className)}>
       <div className="catalog-photo-header__title">
-        <span className="catalog-photo-header__style-and-area">{props.designStyle} {props.homeArea}</span>
-        {' | '}
-        <span className="catalog-photo-header__quality">{props.qualityStandard}</span>
+        {props.loading
+          ? <span className="catalog-photo-header__loading-text">Loading Image...</span>
+          : (
+            <React.Fragment>
+              <span className="catalog-photo-header__style-and-area">{props.designStyle} {props.homeArea}</span>
+              {' | '}
+              <span className="catalog-photo-header__quality">{props.qualityStandard}</span>
+            </React.Fragment>
+          )}
       </div>
     </div>
   );
 };
 
 CatalogPhotoHeader.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  loading: PropTypes.bool
+};
+
+CatalogPhotoHeader.defaultProps = {
+  loading: false
 };
 
 export default CatalogPhotoHeader;
