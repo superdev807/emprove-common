10162ade4ba1f19f8b44d1b11db2396573633a55
nbkhope <renan@emproveit.com>
2018-06-21 16:28:12 -0700

    em-2361 Add test for CloseButton

---
+++ tests/components/CatalogPhotoModal/components/CloseButton/index.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CloseButton from '../../../../../src/components/CatalogPhotoModal/components/CloseButton';
+
+describe('CloseButton component', () => {
+  let component;
+  let props;
+
+  beforeEach(() => {
+    props = {
+    };
+    component = shallow(<CloseButton {...props} />);
+  });
+
+  test('should display an icon button that responds to click based on onClick prop', () => {
+    const onClick = jest.fn();
+    component.setProps({ onClick });
+
+    expect(component.prop('onClick')).toBe(onClick);
+  });
+
+  test('should display a Close icon (X)', () => {
+    expect(component.children().dive().name()).toBe('Close');
+  });
+});
