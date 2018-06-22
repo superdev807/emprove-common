151b49a9020c59d9bf2e8e4c10246455c2351d34
nbkhope <renan@emproveit.com>
2018-06-21 17:17:26 -0700

    em-2361 Add test for CatalogPhoto component

---
+++ tests/components/CatalogPhotoModal/components/CatalogPhoto/index.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CatalogPhoto from '../../../../../src/components/CatalogPhotoModal/components/CatalogPhoto';
+
+describe('CatalogPhoto component', () => {
+  let component;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      imageUrl: '/images/something.png',
+      alt: 'Something :)'
+    };
+    component = shallow(<CatalogPhoto {...props} />);
+  });
+
+  test('should display an image whose src and alt are defined by props', () => {
+    expect(component.prop('src')).toBe(props.imageUrl);
+    expect(component.prop('src')).toBe(props.alt);
+  });
+});
