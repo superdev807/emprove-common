23ee061153eb0fb8e483da35811e672563dd6c92
superdev807 <charles.ferria21@gmail.com>
2018-06-25 17:21:45 -0700

    merging with latest

--- src/components/Footer/components/FooterCopyright/index.js
+++ src/components/Footer/components/FooterCopyright/index.js
@@ -6,28 +6,32 @@ import PropTypes from 'prop-types';
 import Logo from '../../../Logo';
 import './styles.scss';
 
-const FooterCopyright = (props) => {
+const FooterCopyright = props => {
   return (
     <div className="footer-copyright">
       <div className="footer-copyright__logo-container">
         <div className="footer-copyright__logo">
-          <div className="footer-copyright__logo-overline"></div>
+          <div className="footer-copyright__logo-overline" />
           <Logo contractor={props.contractor} version="beta" />
         </div>
       </div>
       <div className="footer-copyright__copyright-declaration">
-        COPYRIGHT © 2018 <a href="/"><strong>EMPROVE, INC.</strong></a> ALL RIGHTS RESERVED.
+        COPYRIGHT © 2018{' '}
+        <a href="/">
+          <strong>EMPROVE, INC.</strong>
+        </a>{' '}
+        ALL RIGHTS RESERVED.
       </div>
     </div>
   );
 };
 
 FooterCopyright.propTypes = {
-  contractor: PropTypes.bool,
+  contractor: PropTypes.bool
 };
 
 FooterCopyright.defaultProps = {
-  contractor: false,
+  contractor: false
 };
 
 export default FooterCopyright;
