839d41f010ae353a48e15b53d0a4fc79711635f6
nbkhope <renan@emproveit.com>
2018-06-26 18:28:25 -0700

    em-2477 Make InformationModal take fullscreen in screens <700px

--- src/components/InformationModal/index.js
+++ src/components/InformationModal/index.js
@@ -42,7 +42,7 @@ const InformationImage = props => {
       <div className="information-image__image-container">
         <img
           className="information-image__image"
-          src={props.image.url}
+        src={props.image.url}
           alt="Information Image"
           width={props.image.displayWidth}
           height={props.image.displayHeight}
@@ -67,12 +67,12 @@ const InformationModal = props => {
   });
 
   return (
-    <Dialog open={props.show} onClose={props.handleHide}>
+    <Dialog classes={{ paper: 'information-modal' }} open={props.show} onClose={props.handleHide}>
       <IconButton className="information-modal-close-button" onClick={props.handleHide}>
         <IconClose />
       </IconButton>
       <DialogTitle>{props.title}</DialogTitle>
-      <DialogContent>
+      <DialogContent className="information-modal__content">
         {props.body.map((paragraph, index) => (
           <Typography key={index} className="information-modal-body-paragraph">
             {paragraph}
