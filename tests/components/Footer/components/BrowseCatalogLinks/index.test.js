a3da68e0a8def28c8889ff3b4c193c3a3b9910fb
nbkhope <renan@emproveit.com>
2018-06-27 09:48:55 -0700

    em-browse Add tests for BrowseCatalogLinks

---
+++ tests/components/Footer/components/BrowseCatalogLinks/index.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import BrowseCatalogLinks from '../../../../../src/components/Footer/components/BrowseCatalogLinks';
+
+describe('BrowseCatalogLinks component', () => {
+  let component;
+  const indices = {
+    homeAreas: 0,
+    homeStyles: 1
+  };
+
+  beforeEach(() => {
+    component = shallow(<BrowseCatalogLinks />);
+  });
+
+  test('should display a list of links to the home areas', () => {
+    component.find('FooterList').at(indices.homeAreas).prop('items').slice(1).forEach(listItem => {
+      expect(listItem.props.to).toMatch(/^\/browse\/any-.+$/);
+    });
+  });
+
+  test('should display a list of links to the home styles', () => {
+    component.find('FooterList').at(indices.homeStyles).prop('items').slice(1).forEach(listItem => {
+      expect(listItem.props.to).toMatch(/^\/browse\/.+-homes$/);
+    });
+  });
+});
