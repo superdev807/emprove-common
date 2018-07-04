4196d367fcdf6a4b228c37e98240131c83751dcc
superdev807 <charles.ferria21@gmail.com>
2018-07-03 17:08:04 -0700

    uncommented/fixed the test

--- tests/components/DatePickerField/index.test.js
+++ tests/components/DatePickerField/index.test.js
@@ -18,7 +18,7 @@ describe('DatePickerField', () => {
     };
     props = {
       ...dispatch,
-      input: {},
+      input: { value: '2018-08-22' },
       meta: {},
       label: '',
       disableDatePast: null
@@ -36,54 +36,52 @@ describe('DatePickerField', () => {
     ).toBe(true);
   });
 
-  // it('should display the date in MM/DD/YYYY format', () => {
-  //   component.setProps({ input: { value: '2018-08-22' } });
+  it('should display the date in MM/DD/YYYY format', () => {
+    component.setProps({ input: { value: '2018-08-22' } });
 
-  //   expect(
-  //     component
-  //       .children()
-  //       .at(0)
-  //       .prop('value')
-  //   ).toBe('08/22/2018');
-  // });
+    expect(
+      component
+        .children()
+        .at(0)
+        .prop('value')
+    ).toBe('08/22/2018');
+  });
 
-  // it('should have viewDate set to input value provided', () => {
-  //   component.setProps({ input: { value: '2018-08-22' } });
+  it('should have viewDate set to input value provided', () => {
+    component.setProps({ input: { value: '2018-08-22' } });
 
-  //   expect(
-  //     component
-  //       .children()
-  //       .at(0)
-  //       .prop('viewDate')
-  //   ).toBe('08/22/2018');
-  // });
+    expect(
+      component
+        .children()
+        .at(0)
+        .prop('viewDate')
+    ).toBe('08/22/2018');
+  });
 
-  // it('should disable dates before disableDatePast', () => {
-  //   component.setProps({ disableDatePast: '2018-06-05' });
-  //   const disablePast = component
-  //     .children()
-  //     .at(0)
-  //     .prop('isValidDate');
+  it('should disable dates before disableDatePast', () => {
+    component.setProps({ disableDatePast: '2018-06-05' });
+    const disablePast = component
+      .children()
+      .at(0)
+      .prop('isValidDate');
 
-  //   expect(disablePast(moment('2018-06-02', 'YYYY-MM-DD'))).toBe(false);
-  //   expect(disablePast(moment('2018-06-22', 'YYYY-MM-DD'))).toBe(true);
-  // });
+    expect(disablePast(moment('2018-06-02', 'YYYY-MM-DD'))).toBe(false);
+    expect(disablePast(moment('2018-06-22', 'YYYY-MM-DD'))).toBe(true);
+  });
 
-  // it('should disable dates before disableDatePast if time provided in moment object', () => {
-  //   component.setProps({ disableDatePast: moment('2018-06-05', 'YYYY-MM-DD') });
-  //   const disablePast = component
-  //     .children()
-  //     .at(0)
-  //     .prop('isValidDate');
+  it('should disable dates before disableDatePast if time provided in moment object', () => {
+    component.setProps({ disableDatePast: moment('2018-06-05', 'YYYY-MM-DD') });
+    const disablePast = component
+      .children()
+      .at(0)
+      .prop('isValidDate');
 
-  //   expect(disablePast(moment('2018-06-02', 'YYYY-MM-DD'))).toBe(false);
-  //   expect(disablePast(moment('2018-06-22', 'YYYY-MM-DD'))).toBe(true);
-  // });
+    expect(disablePast(moment('2018-06-02', 'YYYY-MM-DD'))).toBe(false);
+    expect(disablePast(moment('2018-06-22', 'YYYY-MM-DD'))).toBe(true);
+  });
 
-  // it('should render MUI input component with the input value', () => {
-  //   mounted.setProps({ input: { value: moment('2018-08-22') } });
-
-  //   const muiInput = mounted.find('WithStyles(Input)');
-  //   expect(muiInput.prop('value')).toBe('08/22/2018');
-  // });
+  it('should render MUI input component with the input value', () => {
+    const muiInput = mounted.find('WithStyles(Input)');
+    expect(muiInput.prop('value')).toBe('08/22/2018');
+  });
 });
