00635b94dc6eba960e57a24a0c8d8c76b8a45aca
nbkhope <renan@emproveit.com>
2018-07-19 18:33:22 -0700

    em-2814 Add new PressAndMedia, TipsAndIdeas,  PostsSectionLayout all copied from consumer

---
+++ src/components/AboutUs/components/PostsSectionLayout/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { Link } from 'react-router-dom';
+
+import BlogPost from '~/components/BlogPost';
+import './style.scss';
+
+class PostSectionLayout extends Component {
+  static propTypes = {
+    posts: PropTypes.array,
+    title: PropTypes.object,
+    description: PropTypes.string,
+    linkText: PropTypes.any,
+    linkRoute: PropTypes.string,
+    isLink: PropTypes.bool,
+    postsToShow: PropTypes.number
+  };
+
+  renderPosts(posts) {
+    const { postsToShow } = this.props;
+    return (
+      <CSSTransitionGroup
+        component="div"
+        className="posts-section__transition-div"
+        transitionName="fade"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {posts.map((post, index) => (
+          <div key={index} className="posts-section__item">
+            <BlogPost post={post} />
+          </div>
+        ))}
+      </CSSTransitionGroup>
+    );
+  }
+
+  render() {
+    const { title, posts, linkRoute, description, isLink, linkText } = this.props;
+
+    return (
+      <div className="posts-section" ref={container => (this.container = container)}>
+        <div className="posts-section__title">{title}</div>
+
+        <div>{description}</div>
+        {this.renderPosts(posts)}
+
+        <div className="posts-section__link-container">
+          {isLink ? (
+            <Link to={linkRoute} className="posts-section__link-view-all">
+              {linkText}
+            </Link>
+          ) : (
+            linkText
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostSectionLayout;
---
+++ src/components/AboutUs/components/TipsAndIdeas/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import withPosts from '~/hocs/withPosts';
+import PostsSectionLayout from '../PostsSectionLayout';
+
+class TipsAndIdeas extends Component {
+  render() {
+    const { posts } = this.props;
+    let title = (
+      <div>
+        TIPS&nbsp;
+        <span>&</span>&nbsp;
+        <b>IDEAS</b>
+      </div>
+    );
+
+    return (
+      <PostsSectionLayout
+        description="Get Ideas and Advice for Your Home Improvement Projects"
+        title={title}
+        posts={posts}
+        isLink
+        linkRoute="/blog"
+        linkText="View all tips and ideas"
+      />
+    );
+  }
+}
+
+export default withPosts({
+  params: {
+    number: 3,
+    category: 'featured-on-site',
+    fields: 'ID,title,URL,author,featured_image,excerpt,date'
+  }
+})(TipsAndIdeas);
