71c9114425fba0a7a3f0ca56f7d9a93025a4ce2b
superdev807 <charles.ferria21@gmail.com>
2018-07-19 17:26:21 -0700

    Changed the production hostname to process.env.CONSUMER_DOMAIN

--- src/components/Footer/components/CompanyLinks/index.js
+++ src/components/Footer/components/CompanyLinks/index.js
@@ -5,27 +5,30 @@ import { Link } from 'react-router-dom';
 
 import FooterList from '../FooterList';
 
-const companyLinks = [
-  <a href={process.env.CONSUMER_DOMAIN + "/about-emprove"} target="_blank" rel="noopener noreferrer">About Emprove, Inc.</a>,
-  <a href={process.env.CONSUMER_DOMAIN + "/blog"} target="_blank" rel="noopener noreferrer">Blog</a>,
-  <a href="mailto:contact@emproveit.com">Contact Us</a>,
-  /* <Link to="#">Our Team</Link>,
-  <Link to="#">Investors</Link>,
-  <Link to="#">Press & Media</Link>, */
-  <a href="https://angel.co/emproveit/jobs" target="_blank" rel="noopener noreferrer">
-    Careers
-  </a>,
-  <a href={process.env.CONSUMER_DOMAIN + "/info-center"} target="_blank" rel="noopener noreferrer">Help Center</a>,
-  <a href={process.env.CONSUMER_DOMAIN + "/privacy"} target="_blank" rel="noopener noreferrer">
-    Privacy Policy
-  </a>,
-  <a href={process.env.CONSUMER_DOMAIN + "/terms"} target="_blank" rel="noopener noreferrer">
-    Terms of Service
-  </a>
-];
+//This is converted into a function because the test couldn't inject process.env.CONSUMER_DOMAIN at runtime.
+const companyLinks = () => {
+  return [
+    <a href="/about-emprove">About Emprove, Inc.</a>,
+    <a href={process.env.CONSUMER_DOMAIN + "/blog"} target="_blank" rel="noopener noreferrer">Blog</a>,
+    <a href="mailto:contact@emproveit.com">Contact Us</a>,
+    /* <Link to="#">Our Team</Link>,
+    <Link to="#">Investors</Link>,
+    <Link to="#">Press & Media</Link>, */
+    <a href="https://angel.co/emproveit/jobs" target="_blank" rel="noopener noreferrer">
+      Careers
+    </a>,
+    <a href={process.env.CONSUMER_DOMAIN + "/info-center"} target="_blank" rel="noopener noreferrer">Help Center</a>,
+    <a href={process.env.CONSUMER_DOMAIN + "/privacy"} target="_blank" rel="noopener noreferrer">
+      Privacy Policy
+    </a>,
+    <a href={process.env.CONSUMER_DOMAIN + "/terms"} target="_blank" rel="noopener noreferrer">
+      Terms of Service
+    </a>
+  ]
+};
 
 const CompanyLinks = props => {
-  return <FooterList items={companyLinks} />;
+  return <FooterList items={companyLinks()} />;
 };
 
 export default CompanyLinks;
--- tests/components/Footer/components/CompanyLinks/index.test.js
+++ tests/components/Footer/components/CompanyLinks/index.test.js
@@ -6,6 +6,22 @@ import { shallow } from 'enzyme';
 import CompanyLinks from '~/components/Footer/components/CompanyLinks';
 
 describe('CompanyLinks component', () => {
+  const originalProcess = global.process;
+  const CONSUMER_DOMAIN = 'http://somewhere.com';
+
+  beforeAll(() => {
+    global.process = {
+      ...global.process,
+      env: {
+        CONSUMER_DOMAIN
+      }
+    };
+  });
+
+  afterAll(() => {
+    global.process = originalProcess;
+  });
+
   let component;
 
   beforeEach(() => {
@@ -13,11 +29,12 @@ describe('CompanyLinks component', () => {
   });
 
   test('should display link to about emprove', () => {
-    expect(component.find('FooterList').prop('items')[0].props.href).toEqual(expect.stringContaining('/about-emprove'));
+    //Different footer in about-emprove for each application
+    expect(component.find('FooterList').prop('items')[0].props.href).toBe('/about-emprove');
   });
 
   test('should display link to blog', () => {
-    expect(component.find('FooterList').prop('items')[1].props.href).toEqual(expect.stringContaining('/blog'));
+    expect(component.find('FooterList').prop('items')[1].props.href).toBe(CONSUMER_DOMAIN + '/blog');
   });
 
   test('should display link to contact us', () => {
@@ -29,14 +46,14 @@ describe('CompanyLinks component', () => {
   });
 
   test('should display link to help center', () => {
-    expect(component.find('FooterList').prop('items')[4].props.href).toEqual(expect.stringContaining('/info-center'));
+    expect(component.find('FooterList').prop('items')[4].props.href).toBe(CONSUMER_DOMAIN + '/info-center');
   });
 
   test('should display link to privacy', () => {
-    expect(component.find('FooterList').prop('items')[5].props.href).toEqual(expect.stringContaining('/privacy'));
+    expect(component.find('FooterList').prop('items')[5].props.href).toBe(CONSUMER_DOMAIN + '/privacy');
   });
 
   test('should display link to terms', () => {
-    expect(component.find('FooterList').prop('items')[6].props.href).toEqual(expect.stringContaining('/terms'));
+    expect(component.find('FooterList').prop('items')[6].props.href).toEqual(CONSUMER_DOMAIN + '/terms');
   });
 });
