9f4f1c5cfe9b5ff4fc209d91c04591fbbc9251d1
nbkhope <renan@emproveit.com>
2018-07-20 21:42:16 -0700

    em-2654 Update package json and change prefix for redux constants related to blog

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 
-export const GET_BLOG_POSTS = 'emprove.consumer.GET_BLOG_POSTS';
-export const GET_POST_BY_SLUG = 'emprove.consumer.GET_POST_BY_SLUG';
+export const GET_BLOG_POSTS = 'emprove.common.GET_BLOG_POSTS';
+export const GET_POST_BY_SLUG = 'emprove.common.GET_POST_BY_SLUG';
