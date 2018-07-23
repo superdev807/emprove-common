b7f8cddbe121dca284487c5fcb01bb099e7c5cbe
nbkhope <renan@emproveit.com>
2018-07-23 11:45:36 -0700

    em-2814 Update tests for about us

--- tests/components/AboutUs/index.test.js
+++ tests/components/AboutUs/index.test.js
@@ -22,19 +22,33 @@ describe('AboutUs component', () => {
     expect(component.find('AboutTheCompany').length).toBe(1);
   });
 
+  it('should include sections about the process', () => {
+    expect(component.find('DreamItPartial').length).toBe(1);
+    expect(component.find('CostItPartial').length).toBe(1);
+    expect(component.find('BidItPartial').length).toBe(1);
+  });
+
+  it('should include section about professionals', () => {
+    expect(component.find('WithStyles(ForProfessionals)').length).toBe(1);
+  });
+
+  it('should include section for tips and ideas', () => {
+    expect(component.find('.about-us__tips-and-ideas').length).toBe(1);
+  });
+
   it('should include section about the team', () => {
     expect(component.find('TheTeam').length).toBe(1);
   });
 
-  it('should include section about the product', () => {
-    expect(component.find('OurProduct').length).toBe(1);
+  it('should not include section about the product', () => {
+    expect(component.find('OurProduct').length).toBe(0);
   });
 
   xit('should include section about the investors', () => {
     expect(component.find('OurInvestors').length).toBe(1);
   });
 
-  xit('should include section about the press and media', () => {
-    expect(component.find('PressAndMedia').length).toBe(1);
+  it('should include section about the press and media', () => {
+    expect(component.find('.about-us__press-and-media').length).toBe(1);
   });
 });
