041ce1a37e673517192cfc682a1a5aa1fecec99b
juanortiz10 <juanjo.jjo94@gmail.com>
2018-07-25 17:16:42 -0500

    About us header improvements

--- src/components/AboutUs/components/AboutUsHero/index.js
+++ src/components/AboutUs/components/AboutUsHero/index.js
@@ -4,20 +4,22 @@ import React from 'react';
 
 import './styles.scss';
 
-const WatchHowItWorks = (props) => {
+const WatchHowItWorks = props => {
   return (
     <div className="about-us-hero-watch-how-it-works">
       <img className="about-us-hero-watch-how-it-works__icon" src="/images/ic_play_video.png" onClick={props.onPlayClick} />
-      <div className="about-us-hero-watch-how-it-works__text">WATCH <strong>HOW IT WORKS</strong></div>
+      <div className="about-us-hero-watch-how-it-works__text">
+        WATCH <strong>HOW IT WORKS</strong>
+      </div>
     </div>
   );
-}
+};
 
-const AboutUsHero = (props) => {
+const AboutUsHero = props => {
   return (
     <section className="about-us-hero">
       <div className="about-us-hero__title">
-        Home Improvement <strong>Renewed</strong>
+        Home Improvement <strong>RENEWED</strong>
         <WatchHowItWorks onPlayClick={props.onPlayClick} />
       </div>
     </section>
