7bb53e6d16f6a03a2712da561c78a5f53410c692
juanortiz10 <juanjo.jjo94@gmail.com>
2018-07-27 18:42:56 -0500

    Add testConfig to avoid localStorage errors

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss)$': '<rootDir>mocks/styleMock.js',
-    '^~\\/(.*)$': '<rootDir>/src/$1',
+    '^~\\/(.*)$': '<rootDir>/src/$1'
   },
   setupTestFrameworkScriptFile: '<rootDir>setupTests.js',
+  testURL: 'http://localhost/'
 };
