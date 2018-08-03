daf901e048c575c97e408c15d67fdc48a049edc5
nbkhope <renan@emproveit.com>
2018-08-02 18:41:37 -0700

    em-2863 Add ErrorMessage component

---
+++ tests/components/ErrorMessage/index.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ErrorMessage from '../../../src/components/ErrorMessage';
+
+describe('ErrorMessage component', () => {
+  let component;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      error: 'Failed to do something'
+    };
+    component = shallow(<ErrorMessage {...props} />);
+  });
+
+  test('should display the given error message', () => {
+    expect(component.children().text()).toBe(props.error);
+  });
+
+  test('should display a button to try some operation again', () => {
+    const handleTryAgainClick = jest.fn();
+    component.setProps({
+      tryAgain: true,
+      onTryAgainClick: handleTryAgainClick
+    });
+
+    const button = component.find('WithStyles(Button)');
+    expect(button.length).toBe(1);
+    expect(button.prop('onClick')).toBe(handleTryAgainClick);
+  });
+});
