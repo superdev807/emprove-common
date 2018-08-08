0e884dc2db5d610590d599adee8a52535dfce66a
superdev807 <charles.ferria21@gmail.com>
2018-08-08 11:25:49 -0700

    error boundary created with default fallback component + default error reporter

--- src/hocs/withErrorBoundary/index.js
+++ src/hocs/withErrorBoundary/index.js
@@ -2,34 +2,42 @@
 
 import React, { Component } from 'react';
 
-// import errorReportService from '../services/errorReporter'
+import { trackJsErrorReporter } from '../../../src/hocs/withErrorBoundary/services/errorReporter';
 import FallbackView from './components/FallbackView';
 
-const withErrorBoundary = WrappedComponent => {
+const withErrorBoundary = ({ componentName, customErrorReportService, customFallbackView, fallbackHeader }) => WrappedComponent => {
   return class WithErrorBoundary extends Component {
     constructor(props) {
       super(props);
       this.state = {
         hasError: false,
         error: null,
-        errorInfo: null
+        errorInfo: null,
+        isMobile: this.isPortraitMobile()
       };
     }
 
+    isPortraitMobile() {
+      return (
+        (window.matchMedia('(orientation: portrait)').matches && window.innerWidth <= 600) ||
+        (window.matchMedia('(orientation: landscape)').matches && window.innerWidth <= 960)
+      );
+    }
+
     componentDidCatch(error, errorInfo) {
-      const { customErrorReportService } = this.props;
       this.setState({ hasError: true, error, errorInfo });
 
-      const errorCallback = customErrorReportService || console.log;
-      errorCallback(error, errorInfo, this.props);
+      customErrorReportService && customErrorReportService(error, errorInfo, componentName, this.props);
+      trackJsErrorReporter(error, errorInfo, componentName, this.props);
     }
 
     render() {
-      const FallbackComponent = this.props.customFallbackView || FallbackView;
-
+      const FallbackComponent = customFallbackView || FallbackView;
+      //only show the header if the header is supposed to be hidden for mobile and it's mobile
+      const header = fallbackHeader ? (this.props.hideHeaderForMobile === true && this.state.isMobile ? fallbackHeader : null) : null;
       if (this.state.hasError) {
         const { error, errorInfo } = this.state;
-        return <FallbackComponent {...this.props} error={error} errorInfo={errorInfo} />;
+        return <FallbackComponent {...this.props} error={error} errorInfo={errorInfo} header={header} isMobile={this.state.isMobile} />;
       }
       return <WrappedComponent {...this.props} />;
     }
---
+++ src/hocs/withErrorBoundary/services/errorReporter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export const trackJsErrorReporter = (error, errorInfo, componentName, props) => {
+  // formatting the error to display
+  const stack = 'Error Caught' + errorInfo.componentStack.split(`\n    in`).join(' <');
+  const errorReport = { componentName: componentName || 'Not Provided', type: error.name, message: error.message, stack };
+
+  return console.error('Error caught in React Error Boundary', errorReport);
+};
