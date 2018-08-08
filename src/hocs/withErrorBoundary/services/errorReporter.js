c060159042656bf3280abfb6c2ec95bf5178c42f
superdev807 <charles.ferria21@gmail.com>
2018-08-08 15:29:04 -0700

    revived the default error reporter

--- src/hocs/withErrorBoundary/services/errorReporter.js
+++ src/hocs/withErrorBoundary/services/errorReporter.js
@@ -5,6 +5,5 @@ export const trackJsErrorReporter = (error, errorInfo, componentName, props) => 
   const stack = 'Error Caught' + errorInfo.componentStack.split(`\n    in`).join(' <');
   const errorReport = { componentName: componentName || 'Not Provided', type: error.name, message: error.message, stack };
 
-  // return console.error('Error caught in React Error Boundary', errorReport);
-  return console.log;
+  return console.error('Error caught in React Error Boundary', errorReport);
 };
