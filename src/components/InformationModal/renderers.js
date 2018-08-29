b3a0a2d97b22c4aede4cf1199db47eef75245c84
nbkhope <renan@emproveit.com>
2018-08-29 16:57:22 -0700

    em-3443 Add renderers for InformationModal

---
+++ src/components/InformationModal/renderers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const LinkRenderer = props => {
+  return (
+    <a href={props.href} target="_blank" rel="noopener noreferrer">
+      {props.children}
+    </a>
+  );
+};
+
+export const RawRenderer = props => {
+  console.log('ParagraphRenderer', props);
+  return props.children;
+};
