b5fa6ca72c63a93c3384e13003a1ec16086a4f73
nbkhope <renan@emproveit.com>
2018-08-30 15:16:12 -0700

    em-3443 Add size and margin prop to help link button

---
+++ src/components/HelpLinkButton/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import IconHelpOutline from '@material-ui/icons/HelpOutline';
+
+import './styles.scss';
+
+const HelpLinkButton = (props) => {
+  return (
+    <IconButton
+      className={cx('help-link-button', { 'help-link-button--size-16': props.size === 16, 'help-link-button--left-margin': props.margin === 'left' }, props.className)}
+      component="a"
+      href={`${process.env.HELP_DOMAIN}/${props.path}`}
+      target="_blank">
+      <IconHelpOutline className={cx({ 'help-link-button--size-16': props.size === 16 })}/>
+    </IconButton>
+  );
+};
+
+HelpLinkButton.propTypes = {
+  className: PropTypes.string,
+  margin: PropTypes.string,
+  size: PropTypes.number,
+};
+
+export default HelpLinkButton;
