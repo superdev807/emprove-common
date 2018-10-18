f5d757b4b0ef619619d0cdc372f574ef448dfa45
superdev807 <charles.ferria21@gmail.com>
2018-10-18 15:11:16 -0700

    Updated the test cases

--- tests/utils/timeFunctions.test.js
+++ tests/utils/timeFunctions.test.js
@@ -69,10 +69,10 @@ describe('timeline functions', () => {
       const utcOffsetHours = -1 * utcOffset / 60;
       const rawHour = utcOffsetHours >= 6 ? utcOffsetHours - 6 : 24 - (6 - utcOffsetHours);
       const hour = rawHour < 10 ? `0${rawHour}` : `${rawHour}`;
-      const expectedSubmitProposalsDueDate = utcOffsetHours > 6 ? `2018-07-20T${hour}:00:00.000Z` : `2018-07-19T${hour}:00:00.000Z`;
-      const expectedSiteVisitDueDate = utcOffsetHours > 6 ? `2018-08-01T${hour}:00:00.000Z` : `2018-07-31T${hour}:00:00.000Z`;
-      const expectedFinalBidDueDate = utcOffsetHours > 6 ? `2018-08-04T${hour}:00:00.000Z` : `2018-08-03T${hour}:00:00.000Z`;
-      const expectedAwardDate = utcOffsetHours > 6 ? `2018-08-08T${hour}:00:00.000Z` : `2018-08-07T${hour}:00:00.000Z`;
+      const expectedSubmitProposalsDueDate = utcOffsetHours > 6 ? `2018-07-17T${hour}:00:00.000Z` : `2018-07-16T${hour}:00:00.000Z`;
+      const expectedSiteVisitDueDate = utcOffsetHours > 6 ? `2018-07-27T${hour}:00:00.000Z` : `2018-07-26T${hour}:00:00.000Z`;
+      const expectedFinalBidDueDate = utcOffsetHours > 6 ? `2018-08-01T${hour}:00:00.000Z` : `2018-08-02T${hour}:00:00.000Z`;
+      const expectedAwardDate = utcOffsetHours > 6 ? `2018-08-03T${hour}:00:00.000Z` : `2018-08-02T${hour}:00:00.000Z`;
 
       expect(timeLine.rfpSentDueDate).toBe('2018-07-05 18:00:00');
       expect(timeLine.submitProposalsDueDate.toJSON()).toBe(expectedSubmitProposalsDueDate);
@@ -84,7 +84,7 @@ describe('timeline functions', () => {
 
   describe('awardDateFromRfpSentDueDate', () => {
     it('should return date in YYYY-MM-DD format', () => {
-      expect(awardDateFromRfpSentDueDate(utcDateTime)).toBe('2018-08-07');
+      expect(awardDateFromRfpSentDueDate(utcDateTime)).toBe('2018-08-02');
     });
   });
 
