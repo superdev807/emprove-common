9ab5271a1718bcf487ec4f5b6736b4a097c7c989
Leo Esaki <leo.esaki3@gmail.com>
2018-10-19 22:06:14 +0900

    Add ImpersonationMonitor component

---
+++ src/components/ImpersonationMonitor/index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import Snackbar from '@material-ui/core/Snackbar';
+import Typography from '@material-ui/core/Typography';
+
+moment.relativeTimeThreshold('s', 59);
+moment.relativeTimeThreshold('ss', 0);
+moment.relativeTimeThreshold('m', 40);
+
+const getTimeLeft = exp => moment.duration(moment(exp ? exp * 1000 : undefined).diff(moment()));
+
+class ImpersonationMonitor extends Component {
+  static propTypes = {
+    tokenExp: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    impersonator: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeLeft: getTimeLeft(props.tokenExp).humanize()
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      timeLeft: getTimeLeft(props.tokenExp).humanize()
+    };
+  }
+
+  componentDidMount() {
+    const { impersonator } = this.props;
+    if (impersonator) {
+      this.timer = setInterval(this.handleTimer, 1000);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.impersonator && !this.timer) {
+      this.timer = setInterval(this.handleTimer, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  handleTimer = () => {
+    const timeLeft = getTimeLeft(this.props.tokenExp);
+    if (timeLeft.asSeconds() <= 0) {
+      this.handleClose();
+    } else {
+      this.setState({ timeLeft: timeLeft.humanize() });
+    }
+  };
+
+  handleClose = () => {
+    window.close();
+  };
+
+  render() {
+    const { impersonator } = this.props;
+    const { timeLeft } = this.state;
+
+    return impersonator ? (
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open
+        message={
+          <span className="impersonation-monitor-text">
+            Admin Mode ({impersonator.email}) Expires in {timeLeft}
+          </span>
+        }
+        action={[
+          <Button key="close" color="inherit" onClick={this.handleClose}>
+            Exit
+          </Button>
+        ]}
+      />
+    ) : null;
+  }
+}
+
+export default ImpersonationMonitor;
