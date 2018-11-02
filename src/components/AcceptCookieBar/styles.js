4be39f3851c928d963e7dcae6a4ced5bc7cbc1db
Leo Esaki <leo.esaki3@gmail.com>
2018-11-03 06:23:10 +0900

    Make accept cookie bar UI enhancements

--- src/components/AcceptCookieBar/styles.js
+++ src/components/AcceptCookieBar/styles.js
@@ -7,15 +7,29 @@ export default theme => ({
   content: {
     backgroundColor: theme.palette.text.primary,
     borderRadius: 0,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
     width: '100%',
     maxWidth: '100%'
+  },
+  message: {
+    flex: 1,
+    padding: 0
+  },
+  action: {
+    paddingLeft: theme.spacing.unit,
+    marginRight: 0
   },
   link: {
     color: 'white',
     textDecoration: 'underline'
   },
   action: {
+    color: 'white',
     borderColor: 'white',
-    marginRight: 0
+    marginRight: 0,
+    transition: 'all 0.1s ease',
+    '&:hover, &:active': {
+      color: theme.palette.primary.main
+    }
   }
 });
