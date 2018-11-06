ffb2cce695d3c533893032f05c96dbe271be2b56
superdev807 <charles.ferria21@gmail.com>
2018-11-06 12:07:23 -0800

    make sure userAgent exists

--- src/components/DeviceDetector/helpers.js
+++ src/components/DeviceDetector/helpers.js
@@ -3,6 +3,9 @@
 import MobileDetect from 'mobile-detect';
 
 export const getDeviceType = userAgent => {
-  const md = new MobileDetect(userAgent);
-  return { isMobile: Boolean(md.mobile()), isTablet: Boolean(md.tablet()) };
+  const md = userAgent && new MobileDetect(userAgent);
+  if (md) {
+    return { isMobile: Boolean(md.mobile()), isTablet: Boolean(md.tablet()) };
+  }
+  return { isMobile: false, isTablet: false };
 };
