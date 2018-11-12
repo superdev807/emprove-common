ea9a4db7ebe356bef1142e37768e87b4a508f0f0
Leo Esaki <leo.esaki3@gmail.com>
2018-11-12 12:02:58 +0900

    Make enhancements to IntlProviderTz hoc

--- src/config/intlConfig.js
+++ src/config/intlConfig.js
@@ -20,15 +20,6 @@ export default {
         year: 'numeric',
         timeZoneName: 'short'
       },
-      time: {
-        hour: '2-digit',
-        minute: '2-digit'
-      },
-      timeTz: {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZoneName: 'short'
-      },
       monthAndYear: {
         month: 'long',
         year: 'numeric'
@@ -75,6 +66,17 @@ export default {
       },
       shortWeekday: {
         weekday: 'short'
+      }
+    },
+    time: {
+      hourMinute: {
+        hour: '2-digit',
+        minute: '2-digit'
+      },
+      hourMinuteTz: {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZoneName: 'short'
       }
     },
     number: {
