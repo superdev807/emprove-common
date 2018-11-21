a5cfd5c826c47b68daba92f8395fde86780a07d2
nbkhope <renan@emproveit.com>
2018-11-20 17:20:38 -0800

    em-4784 Make radius of IconMoreMenu 12 so it fills up whole circle and doesnt show border

--- src/icons/IconMoreMenu.js
+++ src/icons/IconMoreMenu.js
@@ -4,8 +4,8 @@ import React from 'react';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
 const IconMoreMenu = props => (
-  <SvgIcon id="prefix__Layer_1" x={0} y={0} viewBox="0 0 24 24" xmlSpace="preserve" {...props}>
-    <circle cx={12} cy={12} r={11.5} />
+  <SvgIcon x={0} y={0} viewBox="0 0 24 24" {...props}>
+    <circle cx={12} cy={12} r={12} />
     <path d="M19.393 19.393V4.607H4.607v14.786h14.786z" fill="none" />
     <path
       fill="#fff"
