3874c69b459898ff3c7144a74e90f44caf3c2e21
Leo Esaki <leo.esaki3@gmail.com>
2018-12-13 12:44:44 +0900

    Make impersonation monitor bar draggable

--- tests/components/ImpersonationMonitor/index.test.js
+++ tests/components/ImpersonationMonitor/index.test.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Draggable from 'react-draggable';
 import Snackbar from '@material-ui/core/Snackbar';
 import { shallow } from 'enzyme';
 
@@ -9,6 +10,7 @@ import ImpersonationMonitor from '~/components/ImpersonationMonitor';
 
 describe('ImpersonationMonitor component', () => {
   let component;
+  let wrapper;
   const realDateNow = Date.now.bind(global.Date);
   const mockedNow = 1539951580;
   const props = {
@@ -27,38 +29,43 @@ describe('ImpersonationMonitor component', () => {
   });
 
   beforeEach(() => {
-    component = shallow(<ImpersonationMonitor {...props} />);
+    wrapper = shallow(<ImpersonationMonitor {...props} />);
   });
 
-  test('should render as <Snackbar />', () => {
-    expect(component.name()).toBe('WithStyles(Snackbar)');
+  test('should render as <Draggable />', () => {
+    expect(wrapper.name()).toBe('Draggable');
   });
 
   test('should render impersonator email', () => {
-    let textWrapper = shallow(component.dive().prop('message')).find('.impersonation-monitor-text');
+    component = wrapper.find(Snackbar).dive();
+    let textWrapper = shallow(component.prop('message')).find('.impersonation-monitor-text');
     expect(textWrapper.text()).toContain('admin@example.com');
-    component.setProps({ impersonator: { email: 'otheradmin@example.com' } });
-    textWrapper = shallow(component.dive().prop('message')).find('.impersonation-monitor-text');
+    wrapper.setProps({ impersonator: { email: 'otheradmin@example.com' } });
+    component = wrapper.find(Snackbar).dive();
+    textWrapper = shallow(component.prop('message')).find('.impersonation-monitor-text');
     expect(textWrapper.text()).toContain('otheradmin@example.com');
   });
 
   test('should render time left', () => {
-    let textWrapper = shallow(component.dive().prop('message')).find('.impersonation-monitor-text');
+    component = wrapper.find(Snackbar).dive();
+    let textWrapper = shallow(component.prop('message')).find('.impersonation-monitor-text');
     expect(textWrapper.text()).toContain('in 30 minutes');
-    component.setProps({ tokenExp: mockedNow + 5 });
-    textWrapper = shallow(component.dive().prop('message')).find('.impersonation-monitor-text');
+    wrapper.setProps({ tokenExp: mockedNow + 5 });
+    component = wrapper.find(Snackbar).dive();
+    textWrapper = shallow(component.prop('message')).find('.impersonation-monitor-text');
     expect(textWrapper.text()).toContain('in 5 seconds');
   });
 
   test('should not render any thing if impersonator is nil', () => {
-    component.setProps({ impersonator: null });
-    expect(component.name()).toEqual(null);
-    component.setProps({ impersonator: undefined });
-    expect(component.name()).toEqual(null);
+    wrapper.setProps({ impersonator: null });
+    expect(wrapper.name()).toEqual(null);
+    wrapper.setProps({ impersonator: undefined });
+    expect(wrapper.name()).toEqual(null);
   });
 
   test('should trigger close action when Exit button is clicked', () => {
-    shallow(component.dive().prop('action')[0]).prop('onClick')();
+    component = wrapper.find(Snackbar).dive();
+    shallow(component.prop('action')[0]).prop('onClick')();
     expect(props.onExit).toHaveBeenCalled();
   });
 });
