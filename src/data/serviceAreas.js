21e71511a923031e738280b1f22a84b53b9a0f9b
juanortiz10 <juanjo.jjo94@gmail.com>
2019-01-28 14:19:45 -0600

    Add more supported and coming soon areas to footer

--- src/data/serviceAreas.js
+++ src/data/serviceAreas.js
@@ -1,2 +1,16 @@
-export const serviceAreas = ['Greater Las Vegas, NV', 'Greater Los Angeles, CA'];
-export const comingSoonAreas = ['Greater San Diego, CA', 'Greater Phoenix, AZ'];
+export const serviceAreas = [
+  'Greater Las Vegas, NV',
+  'Greater Los Angeles, CA',
+  'Greater San Bernardino, CA',
+  'Greater Riverside, CA',
+  'Greater San Diego, CA',
+  'Greater Phoenix, AZ'
+];
+
+export const comingSoonAreas = [
+  'Greater San Francisco Bay Area, CA',
+  'Greater Sacramento, CA',
+  'Greater Austin, TX',
+  'Greater Houston, TX',
+  'Greater Dallas, TX'
+];
