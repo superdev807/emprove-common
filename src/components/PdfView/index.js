58d65f1973da92a5df9b0792d823dc789c9b888b
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2019-02-06 20:15:59 +0900

    Add page buttons on pdf view

--- src/components/PdfView/index.js
+++ src/components/PdfView/index.js
@@ -2,9 +2,18 @@
 
 import React, { Component } from 'react';
 import cx from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import IconNext from '@material-ui/icons/ChevronRight';
+import IconPrev from '@material-ui/icons/ChevronLeft';
+import IconFirst from '@material-ui/icons/FirstPage';
+import IconLast from '@material-ui/icons/LastPage';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { Document, Page } from 'react-pdf';
+import { isMobileSelector } from '../../redux/selectors';
 import { withStyles } from '@material-ui/core/styles';
 
 import styles from './styles';
@@ -12,14 +21,16 @@ import styles from './styles';
 class PdfView extends Component {
   static propTypes = {
     className: PropTypes.string,
-    source: PropTypes.string
+    source: PropTypes.string,
+    showPageButtons: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
     this.state = {
       numPages: 0,
-      width: 0
+      width: 0,
+      currentPage: 1
     };
   }
 
@@ -52,22 +63,63 @@ class PdfView extends Component {
     this.pdfWrapper = ref;
   };
 
-  render() {
-    const { classes, source, show, handleHide } = this.props;
-    const { width } = this.state;
+  handlePrevPage = () => {
+    this.setState({ currentPage: Math.max(this.state.currentPage - 1, 1) });
+  };
 
-    const pdfPages = Array.from(new Array(this.state.numPages), (el, index) => (
-      <Paper className={classes.paper} component={Page} key={`page_${index + 1}`} pageNumber={index + 1} width={width} />
-    ));
+  handleNextPage = () => {
+    this.setState({ currentPage: Math.min(this.state.currentPage + 1, this.state.numPages) });
+  };
+
+  handleFirstPage = () => {
+    this.setState({ currentPage: 1 });
+  };
+
+  handleLastPage = () => {
+    this.setState({ currentPage: this.state.numPages });
+  };
+
+  render() {
+    const { classes, source, show, showPageButtons, handleHide, isMobile } = this.props;
+    const { currentPage, numPages, width } = this.state;
+    const hasButtons = showPageButtons && isMobile;
 
     return (
-      <div ref={this.handleWrapperRef} className={cx(classes.root, this.props.className)}>
-        <Document file={source} onLoadSuccess={this.handleDocumentLoad}>
-          {pdfPages}
-        </Document>
+      <div ref={this.handleWrapperRef} className={cx(classes.root, this.props.className, { [classes.mobileRoot]: hasButtons })}>
+        <div className={cx({ [classes.mobileDocument]: hasButtons })}>
+          <Document file={source} onLoadSuccess={this.handleDocumentLoad}>
+            {hasButtons ? (
+              <Paper className={classes.paper} component={Page} key={`page_${currentPage}`} pageNumber={currentPage} width={width} />
+            ) : (
+              Array.from(new Array(numPages), (el, index) => (
+                <Paper className={classes.paper} component={Page} key={`page_${index + 1}`} pageNumber={index + 1} width={width} />
+              ))
+            )}
+          </Document>
+        </div>
+        {hasButtons && (
+          <div className={classes.buttonContainer}>
+            <IconButton onClick={this.handleFirstPage}>
+              <IconFirst />
+            </IconButton>
+            <IconButton onClick={this.handlePrevPage}>
+              <IconPrev />
+            </IconButton>
+            <IconButton onClick={this.handleNextPage}>
+              <IconNext />
+            </IconButton>
+            <IconButton onClick={this.handleLastPage}>
+              <IconLast />
+            </IconButton>
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default withStyles(styles)(PdfView);
+const selector = createStructuredSelector({
+  isMobile: isMobileSelector
+});
+
+export default compose(withStyles(styles), connect(selector))(PdfView);
--- src/components/PdfView/styles.js
+++ src/components/PdfView/styles.js
@@ -4,7 +4,22 @@ export default theme => ({
   root: {
     overflow: 'hidden'
   },
+  mobileRoot: {
+    height: '100%',
+    position: 'relative'
+  },
   paper: {
     margin: `${theme.spacing.unit}px 0`
+  },
+  mobileDocument: {
+    height: 'calc(100% - 48px)',
+    overflow: 'auto'
+  },
+  buttonContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    textAlign: 'center'
   }
 });
