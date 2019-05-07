e3fa8e3327e76b413b53ee3eb14d396872085b14
superdev807 <charles.ferria21@gmail.com>
2019-05-07 10:57:55 -0700

    bbb logo added

--- src/components/Footer/components/FooterCopyright/index.js
+++ src/components/Footer/components/FooterCopyright/index.js
@@ -9,18 +9,37 @@ import './styles.scss';
 const FooterCopyright = props => {
   return (
     <div className="footer-copyright">
-      <div className="footer-copyright__logo-container">
-        <div className="footer-copyright__logo">
-          <div className="footer-copyright__logo-overline" />
-          <Logo imageClass="footer-copyright__logo-image" contractor={props.contractor} />
+      <div className="footer-copyright__container">
+        <div className="footer-copyright__logo-container">
+          <div className="footer-copyright__logo">
+            <div className="footer-copyright__logo-overline" />
+            <Logo imageClass="footer-copyright__logo-image" contractor={props.contractor} />
+          </div>
+        </div>
+        <div className="footer-copyright__copyright-declaration">
+          © 2019{' '}
+          <a href="/">
+            <strong>EMPROVE, LLC.</strong>
+          </a>{' '}
+          ALL RIGHTS RESERVED.
         </div>
       </div>
-      <div className="footer-copyright__copyright-declaration">
-        © 2019{' '}
-        <a href="/">
-          <strong>EMPROVE, LLC.</strong>
-        </a>{' '}
-        ALL RIGHTS RESERVED.
+      <div className="footer-copyright__container footer-copyright__container--bbb">
+        <a
+          id="bbblink"
+          className="sevtbum"
+          href="https://www.bbb.org/us/nv/las-vegas/profile/home-improvement/emprove-1086-90063688#bbbseal"
+          title="Emprove, Inc., Home Improvement, Las Vegas, NV"
+          style={{ display: 'block', position: 'relative', overflow: 'hidden', width: 60, height: 98, margin: 0, padding: 0 }}>
+          <img
+            style={{ padding: 0, border: 'none' }}
+            id="bbblinkimg"
+            src="https://seal-southernnevada.bbb.org/logo/sevtbum/emprove-90063688.png"
+            width={120}
+            height={98}
+            alt="Emprove, Inc., Home Improvement, Las Vegas, NV"
+          />
+        </a>
       </div>
     </div>
   );
