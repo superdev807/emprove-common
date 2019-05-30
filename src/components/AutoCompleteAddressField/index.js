493d9330a37e8f19ed99fd76a84a6dbcf1337c0b
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2019-05-30 12:35:41 +0900

    Use google api to auto complete address

--- src/components/AutoCompleteAddressField/index.js
+++ src/components/AutoCompleteAddressField/index.js
@@ -1,93 +1,118 @@
 'use strict';
 
 import React, { Component } from 'react';
-import AlgoliaPlaces from 'algolia-places-react';
+import cx from 'classnames';
+import get from 'lodash/get';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { propsChanged } from '../../utils/pureFunctions';
+import PlacesAutocomplete, { geocodeByPlaceId } from 'react-places-autocomplete';
+
+import loadScript from '../../utils/loadScript';
+import { makeCancelable } from '../../utils/promiseFunctions';
 
 import './styles.scss';
 
-class AutoCompleteAddressField extends Component {
-  static propTypes = {
-    searchType: PropTypes.string,
-    type: PropTypes.oneOf(['text', 'button'])
-  };
-
-  static defaultPropTypes = {
-    searchType: 'address',
-    type: 'text'
-  };
-
+class AddressField extends Component {
   constructor(props) {
     super(props);
 
-    this.algoliaPlaces = React.createRef();
+    this.state = {
+      googleReady: Boolean(window.google)
+    };
+    this.cancelableLoadScript = null;
   }
 
   componentDidMount() {
-    this.setAddress();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (propsChanged(['input.value', 'type'], prevProps, this.props)) {
-      this.setAddress();
+    if (!window.google) {
+      this.cancelableLoadScript = makeCancelable(
+        loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.GEOCODE_MAP_API_KEY}&libraries=places`)
+      );
+      this.cancelableLoadScript.promise.then(() => {
+        this.setState({
+          googleReady: true
+        });
+      });
     }
   }
 
-  setAddress() {
-    const { input } = this.props;
-
-    if (typeof input.value === 'string' && this.algoliaPlaces.current) {
-      this.algoliaPlaces.current.autocomplete.setVal(input.value);
-    }
+  componentWillUnmount() {
+    this.cancelableLoadScript && this.cancelableLoadScript.cancel();
   }
 
-  handleChange = payload => {
-    this.props.input.onChange(payload.suggestion);
+  handleChange = address => {
+    this.props.input.onChange(address);
   };
 
-  handleClear = () => {
-    this.props.input.onChange(null);
+  handleSelect = (address, placeId) => {
+    geocodeByPlaceId(placeId)
+      .then(results => {
+        const found = results[0].address_components.find(item => item.types[0] === 'postal_code');
+        if (found) {
+          this.props.input.onBlur({ name: address, postcode: found.long_name });
+        } else {
+          this.props.input.onBlur(address);
+        }
+      })
+      .catch(error => {
+        this.props.input.onBlur(address);
+      });
   };
 
   render() {
     const {
       className,
-      classes,
       errorMessageClass,
       hideErrorText,
       input,
-      placeholder,
-      meta: { touched, error },
-      searchType,
-      type
+      type,
+      meta: { touched, error }
     } = this.props;
 
-    return (
-      <FormControl className={cx('auto-complete-address-field', className)} error={touched && !!error}>
-        {type === 'text' ? (
-          <AlgoliaPlaces
-            ref={this.algoliaPlaces}
-            placeholder={placeholder}
-            options={{
-              appId: process.env.ALGOLIA_PLACES_APP_ID,
-              apiKey: process.env.ALGOLIA_PLACES_API_KEY,
-              type: searchType,
-              countries: ['us']
-            }}
-            onChange={this.handleChange}
-            onClear={this.handleClear}
-          />
-        ) : (
-          <div className="button">{typeof input.value === 'string' ? input.value : ''}</div>
-        )}
-        {!hideErrorText && touched && error && <FormHelperText className={errorMessageClass}>{error}</FormHelperText>}
-      </FormControl>
-    );
+    const errorEl = !hideErrorText && touched && error ? <FormHelperText className={errorMessageClass}>{error}</FormHelperText> : null;
+
+    if (type === 'text') {
+      return this.state.googleReady ? (
+        <PlacesAutocomplete value={get(input, 'value.name', input.value)} onChange={this.handleChange} onSelect={this.handleSelect}>
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <FormControl className={cx('auto-complete-address-field', className)} error={touched && !!error}>
+              <div className="auto-complete-address-field__formControl">
+                <input
+                  {...getInputProps({
+                    className: 'auto-complete-address-field__input'
+                  })}
+                />
+              </div>
+              {suggestions.length > 0 && (
+                <div className="auto-complete-address-field__dropdown">
+                  {/*loading && <div>Loading...</div>*/}
+                  {suggestions.map(suggestion => (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className: cx('auto-complete-address-field__dropdown-item', {
+                          'auto-complete-address-field__dropdown-item--active': suggestion.active
+                        })
+                      })}>
+                      <span>{suggestion.description}</span>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {errorEl}
+            </FormControl>
+          )}
+        </PlacesAutocomplete>
+      ) : null;
+    } else {
+      return (
+        <FormControl className={cx('auto-complete-address-field', className)} error={touched && !!error}>
+          <div className="auto-complete-address-field__formControl">
+            <div className="auto-complete-address-field__input">{input.value}</div>
+          </div>
+          {errorEl}
+        </FormControl>
+      );
+    }
   }
 }
 
-export default AutoCompleteAddressField;
+export default AddressField;
